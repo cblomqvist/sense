a6cb1d8ebe667956f5d1ad2bcc34d6a5103ce1a8
Christer Blomqvist <cblomqvist@spotify.com>
2016-05-11 13:41:10 +0200

    Add a button and text

--- dashboard/src/main/java/com/mechanitis/demo/sense/client/DashboardController.java
+++ dashboard/src/main/java/com/mechanitis/demo/sense/client/DashboardController.java
@@ -4,13 +4,18 @@ import com.mechanitis.demo.sense.client.mood.HappinessController;
 import com.mechanitis.demo.sense.client.mood.MoodController;
 import com.mechanitis.demo.sense.client.user.HellohackController;
 import com.mechanitis.demo.sense.client.user.LeaderboardController;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class DashboardController {
     @FXML private MoodController moodController;
     @FXML private HappinessController happyController;
     @FXML private LeaderboardController leaderboardController;
     @FXML private HellohackController hellohackController;
+
+    @FXML private Text actiontarget;
 
     public MoodController getMoodController() {
         return moodController;
@@ -26,5 +31,9 @@ public class DashboardController {
 
     public HellohackController getHellohackController() {
         return hellohackController;
+    }
+
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
     }
 }
