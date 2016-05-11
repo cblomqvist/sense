5cd69d788eb71cfa51189a56227862f0ae83c6a8
Christer Blomqvist <cblomqvist@spotify.com>
2016-05-11 14:06:34 +0200

    Send ping to Apollo service

--- dashboard/src/main/java/com/mechanitis/demo/sense/client/user/HelloHackController.java
+++ dashboard/src/main/java/com/mechanitis/demo/sense/client/user/HelloHackController.java
@@ -1,5 +1,12 @@
 package com.mechanitis.demo.sense.client.user;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
@@ -13,7 +20,20 @@ public class HellohackController {
 
   @FXML
   protected void handleActionButtonAction(ActionEvent event) {
-    actiontarget.setText("Action button pressed");
+
+    OkHttpClient okClient = new OkHttpClient();
+    final Request request = new Request.Builder().url("http://localhost:8080/ping").build();
+    final Call call = okClient.newCall(request);
+    try {
+      final Response response = call.execute();
+      final int code = response.code();
+      actiontarget.setText("Response code: "+code+", message: "+response.body().string());
+
+    } catch (IOException e) {
+      actiontarget.setText(String.format("Action button pressed but something went wrong: %s", e.getMessage()));
+      e.printStackTrace();
+    }
+
   }
 
 }
